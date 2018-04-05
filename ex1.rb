17e340628c68bb3d51ae23204c515d3cfc46f93d
behrami <behram1993@gmail.com>
2018-04-05 13:55:09 -0400

    finished part1

---
+++ ex1.rb
@@ -0,0 +1,23 @@
+temp_hash= { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+
+# [:data][:rooms]
+# loop in array --- if [:room_number]=201 return :capacity
+
+temp_array=temp_hash[:data][:rooms]
+
+temp_array.each do |value|
+  if value[:room_number] == "201"
+    return p value[:capacity]
+  end
+end
